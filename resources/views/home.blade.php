a3e351e02dc8acd33ef24901a05c9ca612e56a4b
KostadinStefanov <kosstefanov@gmail.com>
2016-04-11 02:29:27 +0300

    sha si lqgam

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,8 @@
 @extends('layouts.main')
 @section('promo')
-<section id="promo-main" xmlns="http://www.w3.org/1999/html">
-    <div id="promo-details">
-        <h1>Challange YourSelf</h1>
-        <p>Most Addicting<br />
-            MMORPG Game of the Year !!!.</p><br />
-        <a href="/game" class="default-btn">Play Now</a>
-    </div>
-</section><!-- promo -->
+            <div>
+                <a href="/game"><img src="img/slide.png"> </a>
+            </div>
 @stop
 @section('content')
 @stop
